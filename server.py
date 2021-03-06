50e63ed892713495fedf272a05217431e4abdc69
Lioueq <71265562+Lioueq@users.noreply.github.com>
2021-03-06 14:41:44 +0300

    4

--- server.py
+++ server.py
@@ -25,6 +25,7 @@ def img():
                 <html lang="en">
                   <head>
                     <title>Привет, Марс!</title>
+                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
                   </head>
                   <body>
                     <h1>Жди нас, Марс!</h1>
@@ -36,7 +37,27 @@ def img():
 
 @app.route('/promotion_image')
 def promotion_img():
-    pass
+    return f'''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+                    <title>Привет, Марс!</title>
+                  </head>
+                  <body>
+                    <h1>Жди нас, Марс!</h1>
+                    <img src="static/img/MARS.png" alt="Фото Марса">
+                    <div class="alert alert-dark" role="alert">Человечество вырастает из детства.</div>
+                    <div class="alert alert-success" role="alert">Человечеству мала одна планета.</div>
+                    <div class="alert alert-secondary" role="alert">Мы сделаем обитаемыми безжизненные пока планеты.</div>
+                    <div class="alert alert-warning" role="alert">И начнем с Марса!</div>
+                    <div class="alert alert-danger" role="alert">Присоединяйся!</div>
+                  </body>
+                </html>'''
 
 
 if __name__ == '__main__':
