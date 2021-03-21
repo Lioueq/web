2618b7ae801f5697983c5dc848c01f7cafb2e7e8
Lioueq <71265562+Lioueq@users.noreply.github.com>
2021-03-21 16:41:38 +0300

    7

--- server.py
+++ server.py
@@ -5,129 +5,184 @@ app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/astronaut_selection', methods=['POST', 'GET'])
-def index():
-    if request.method == 'GET':
+@app.route('/choice/<planet_name>')
+def planet_info(planet_name):
+    if planet_name.capitalize() == 'Марс':
         return f'''<!doctype html>
-                        <html lang="en">
-                          <head>
-                            <meta charset="utf-8">
-                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                            <link rel="stylesheet"
-                            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
-                            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-                            crossorigin="anonymous">
-                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
-                            <title>Пример формы</title>
-                          </head>
-                          <body>
-                            <h1>Анкета претендента</h1>
-                            <h2>на участие в миссии</h2>
-                            <div>
-                                <form class="login_form" method="post">
-                                    <input type="surname" class="form-control" id="surname" placeholder="Введите фамилию" name="surname">
-                                    <input type="name" class="form-control" id="name" placeholder="Введите имя" name="name">
-                                    <input type="email" class="form-control form-control-email" id="email" aria-describedby="emailHelp" placeholder="Введите адрес почты" name="email">
-                                    <div class="form-group">
-                                        <label for="classSelect">Какое у Вас образование?</label>
-                                        <select class="form-control" id="classSelect" name="class">
-                                          <option>Начальное</option>
-                                          <option>Среднее</option>
-                                          <option>Высшее</option>
-                                        </select>
-                                     </div>
-                                    <div class="form-group">
-                                        <label for="form-check">Какие у Вас есть профессии?</label>
-                                        <div class="form-check">
-                                          <input class="form-check-input" type="checkbox" name="prof" id="prof1">
-                                          <label class="form-check-label" for="prof1">
-                                            Инженер-исследователь
-                                          </label>
-                                        </div>
-                                        <div class="form-check">
-                                          <input class="form-check-input" type="checkbox" name="prof" id="prof2">
-                                          <label class="form-check-label" for="prof2">
-                                            Инженер-строитель
-                                          </label>
-                                        </div>
-                                        <div class="form-check">
-                                          <input class="form-check-input" type="checkbox" name="prof" id="prof3">
-                                          <label class="form-check-label" for="prof3">
-                                            Пилот
-                                          </label>
-                                        </div>
-                                        <div class="form-check">
-                                          <input class="form-check-input" type="checkbox" name="prof" id="prof4">
-                                          <label class="form-check-label" for="prof4">
-                                            Метеоролог
-                                          </label>
-                                        </div>
-                                        <div class="form-check">
-                                          <input class="form-check-input" type="checkbox" name="prof" id="prof5">
-                                          <label class="form-check-label" for="prof5">
-                                            Инженер по жизнеобеспечению
-                                          </label>
-                                        </div>
-                                        <div class="form-check">
-                                          <input class="form-check-input" type="checkbox" name="prof" id="prof6">
-                                          <label class="form-check-label" for="prof6">
-                                            Инженер по радиационной защите
-                                          </label>
-                                        </div>
-                                        <div class="form-check">
-                                          <input class="form-check-input" type="checkbox" name="prof" id="prof7">
-                                          <label class="form-check-label" for="prof7">
-                                            Врач
-                                          </label>
-                                        </div>
-                                        <div class="form-check">
-                                          <input class="form-check-input" type="checkbox" name="prof" id="prof8">
-                                          <label class="form-check-label" for="prof8">
-                                            Экзобиолог
-                                          </label>
-                                        </div>                                        
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="form-check">Укажите пол</label>
-                                        <div class="form-check">
-                                          <input class="form-check-input" type="radio" name="sex" id="male" value="male" checked>
-                                          <label class="form-check-label" for="male">
-                                            Мужской
-                                          </label>
-                                        </div>
-                                        <div class="form-check">
-                                          <input class="form-check-input" type="radio" name="sex" id="female" value="female">
-                                          <label class="form-check-label" for="female">
-                                            Женский
-                                          </label>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="about">Почему Вы принять участие в миссии?</label>
-                                        <textarea class="form-control" id="about" rows="3" name="about"></textarea>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="photo">Приложите фотографию</label>
-                                        <input type="file" class="form-control-file" id="photo" name="file">
-                                    </div>
-                                    <div class="form-group form-check">
-                                        <input type="checkbox" class="form-check-input" id="acceptRules" name="accept">
-                                        <label class="form-check-label" for="acceptRules">Готовы остаться на Марсе?</label>
-                                    </div>
-                                    <button type="submit" class="btn btn-primary">Записаться</button>
-                                </form>
-                            </div>
-                          </body>
-                        </html>'''
-    elif request.method == 'POST':
-        print(request.form['email'])
-        print(request.form['password'])
-        print(request.form['class'])
-        print(request.form['file'])
-        print(request.form['about'])
-        print(request.form['accept'])
-        print(request.form['sex'])
-        return "Форма отправлена"
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <link rel="stylesheet" 
+                        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                        crossorigin="anonymous">
+                        <title>Варианты выбора</title>
+                      </head>
+                      <body>
+                        <h1>Мое предложение: {planet_name.capitalize()}</h1>
+                        <h2>
+                        <div class="alert alert-dark" role="alert">Эта планета близка к Земле;</div>
+                        <div class="alert alert-success" role="alert">На ней много необходимых ресурсов;</div>
+                        <div class="alert alert-secondary" role="alert">На ней есть вода и атмосфера;</div>
+                        <div class="alert alert-warning" role="alert">На ней есть небольшое магнитное поле;</div>
+                        <div class="alert alert-danger" role="alert">Наконец она просто красива!</div>
+                        </h2>
+                      </body>
+                    </html>'''
+    elif planet_name.capitalize() == 'Меркурий':
+        return f'''<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <link rel="stylesheet" 
+                        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                        crossorigin="anonymous">
+                        <title>Варианты выбора</title>
+                      </head>
+                      <body>
+                        <h1>Мое предложение: {planet_name.capitalize()}</h1>
+                        <h2>
+                        <div class="alert alert-dark" role="alert">Эта планета близка к солнцу;</div>
+                        <div class="alert alert-success" role="alert">Самая маленькая планета;</div>
+                        <div class="alert alert-secondary" role="alert">На ней нет воды и атмосферы;</div>
+                        <div class="alert alert-warning" role="alert">Внешне похожа на Луну;</div>
+                        <div class="alert alert-danger" role="alert">Наконец она просто красива!</div>
+                        </h2>
+                      </body>
+                    </html>'''
+    elif planet_name.capitalize() == 'Венера':
+        return f'''<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <link rel="stylesheet" 
+                        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                        crossorigin="anonymous">
+                        <title>Варианты выбора</title>
+                      </head>
+                      <body>
+                        <h1>Мое предложение: {planet_name.capitalize()}</h1>
+                        <h2>
+                        <div class="alert alert-dark" role="alert">Эта планета вторая в Солнечной системе;</div>
+                        <div class="alert alert-success" role="alert">Шестая по размеру планета;</div>
+                        <div class="alert alert-secondary" role="alert">На ней нет воды, но есть атмосфера;</div>
+                        <div class="alert alert-warning" role="alert">На ней есть небольшое магнитное поле;</div>
+                        <div class="alert alert-danger" role="alert">Наконец она просто красива!</div>
+                        </h2>
+                      </body>
+                    </html>'''
+    elif planet_name.capitalize() == 'Земля':
+        return f'''<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <link rel="stylesheet" 
+                        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                        crossorigin="anonymous">
+                        <title>Варианты выбора</title>
+                      </head>
+                      <body>
+                        <h1>Мое предложение: {planet_name.capitalize()}</h1>
+                        <h2>
+                        <div class="alert alert-dark" role="alert">Эта планета третья в Солнечной системе;</div>
+                        <div class="alert alert-success" role="alert">Единственная планета населенная живыми организмами;</div>
+                        <div class="alert alert-secondary" role="alert">На ней есть вода и атмосфера;</div>
+                        <div class="alert alert-warning" role="alert">На ней есть магнитное поле;</div>
+                        <div class="alert alert-danger" role="alert">Наконец она просто красива!</div>
+                        </h2>
+                      </body>
+                    </html>'''
+    elif planet_name.capitalize() == 'Юпитер':
+        return f'''<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <link rel="stylesheet" 
+                        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                        crossorigin="anonymous">
+                        <title>Варианты выбора</title>
+                      </head>
+                      <body>
+                        <h1>Мое предложение: {planet_name.capitalize}</h1>
+                        <h2>
+                        <div class="alert alert-dark" role="alert">Эта планета пятая в Солнечной системе;</div>
+                        <div class="alert alert-success" role="alert">Самая большая планета в Солнечной системе;</div>
+                        <div class="alert alert-secondary" role="alert">На ней есть вода и атмосфера;</div>
+                        <div class="alert alert-warning" role="alert">На ней есть магнитное поле;</div>
+                        <div class="alert alert-danger" role="alert">Наконец она просто красива!</div>
+                        </h2>
+                      </body>
+                    </html>'''
+    elif planet_name.capitalize() == 'Сатурн':
+        return f'''<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <link rel="stylesheet" 
+                        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                        crossorigin="anonymous">
+                        <title>Варианты выбора</title>
+                      </head>
+                      <body>
+                        <h1>Мое предложение: {planet_name.capitalize}</h1>
+                        <h2>
+                        <div class="alert alert-dark" role="alert">Эта планета шестая в Солнечной системе;</div>
+                        <div class="alert alert-success" role="alert">Вторая по размеру планета в Солнечной системе;</div>
+                        <div class="alert alert-secondary" role="alert">На ней есть атмосфера, но нет воды;</div>
+                        <div class="alert alert-warning" role="alert">На ней есть магнитное поле;</div>
+                        <div class="alert alert-danger" role="alert">Наконец она просто красива!</div>
+                        </h2>
+                      </body>
+                    </html>'''
+    elif planet_name.capitalize() == 'Уран':
+        return f'''<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <link rel="stylesheet" 
+                        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                        crossorigin="anonymous">
+                        <title>Варианты выбора</title>
+                      </head>
+                      <body>
+                        <h1>Мое предложение: {planet_name.capitalize}</h1>
+                        <h2>
+                        <div class="alert alert-dark" role="alert">Эта планета седьмая в Солнечной системе;</div>
+                        <div class="alert alert-success" role="alert">Третья по размеру планета в Солнечной системе;</div>
+                        <div class="alert alert-secondary" role="alert">На ней есть вода и атмосфера;</div>
+                        <div class="alert alert-warning" role="alert">На ней есть магнитное поле;</div>
+                        <div class="alert alert-danger" role="alert">Наконец она просто красива!</div>
+                        </h2>
+                      </body>
+                    </html>'''
+    elif planet_name.capitalize() == 'Нептун':
+        return f'''<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <link rel="stylesheet" 
+                        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                        crossorigin="anonymous">
+                        <title>Варианты выбора</title>
+                      </head>
+                      <body>
+                        <h1>Мое предложение: {planet_name.capitalize}</h1>
+                        <h2>
+                        <div class="alert alert-dark" role="alert">Последняя планета в Солнечной системе;</div>
+                        <div class="alert alert-success" role="alert">Четвертая по размеру планета в Солнечной системе;</div>
+                        <div class="alert alert-secondary" role="alert">На ней есть вода и атмосфера;</div>
+                        <div class="alert alert-warning" role="alert">На ней есть магнитное поле;</div>
+                        <div class="alert alert-danger" role="alert">Наконец она просто красива!</div>
+                        </h2>
+                      </body>
+                    </html>'''
 
 
 if __name__ == '__main__':
