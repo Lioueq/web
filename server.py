19be636b192d1d220d2e23da72bd1cf8d477f0a2
Lioueq <71265562+Lioueq@users.noreply.github.com>
2021-03-23 17:56:44 +0300

    12

--- server.py
+++ server.py
@@ -1,15 +1,17 @@
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/training/<prof>')
-def index(prof):
-    return render_template('index.html', prof=prof)
+@app.route('/list_prof/<list>')
+def index(list):
+    list_profs = ['инженер-исследователь', 'пилот', 'строитель', 'экзобиолог', 'врач', 'инженер по терраформированию',
+                  'климатолог', 'специалист по радиационной защите', 'астрогеолог', 'гляциолог',
+                  'инженер жизнеобеспечения', 'метеоролог', 'оператор марсохода', 'киберинженер', 'штурман',
+                  'пилот дронов']
+    return render_template('index.html', mode=list, list_profs=list_profs)
 
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
-    print('1')
