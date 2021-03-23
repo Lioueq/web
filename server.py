4e5a7abb4dab811cf41a7d926b66ef7fc58801ae
Lioueq <71265562+Lioueq@users.noreply.github.com>
2021-03-23 14:05:39 +0300

    9

--- server.py
+++ server.py
@@ -1,32 +1,46 @@
-from flask import Flask
+from flask import Flask, url_for, request
+import os
 
 
 app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/results/<nickname>/<int:level>/<float:rating>')
-def planet_info(nickname, level, rating):
-    return f'''<!doctype html>
-                <html lang="en">
-                  <head>
-                    <meta charset="utf-8">
-                    <link rel="stylesheet" 
-                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
-                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
-                    crossorigin="anonymous">
-                    <title>Результаты</title>
-                  </head>
-                  <body>
-                    <h1>Результаты отбора</h1>
-                    <h2>
-                    <div class="alert alert" role="alert">Претендента на участие в миссии {nickname}:</div>
-                    <div class="alert alert-success" role="alert">Поздравляем! Ваш рейтинг после {level} этапа отбора</div>
-                    <div class="alert alert" role="alert">составляет {rating}!</div>
-                    <div class="alert alert-warning" role="alert">Желаем удачи!</div>
-                    </h2>
-                  </body>
-                </html>'''
+@app.route('/load_photo', methods=['POST', 'GET'])
+def load_photo():
+    if request.method == 'GET':
+        return f'''<!doctype html>
+                            <html lang="en">
+                              <head>
+                                <meta charset="utf-8">
+                                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                                 <link rel="stylesheet"
+                                 href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                                 integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                                 crossorigin="anonymous">
+                                <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                                <title>Пример загрузки файла</title>
+                              </head>
+                              <body>
+                                <h1>Загрузка фотографии</h1>
+                                <h2>для участия в миссии</h2>
+                                <form class="login_form" method="post" enctype="multipart/form-data">
+                                    <div class="form-group">
+                                        <label for="photo">Приложите фотографию</label>
+                                        <input type="file" class="form-control-file" id="photo" name="file">
+                                    </div>
+                                    <div class="form-group">
+                                        <img src="static/img/self_image.png" height="400">
+                                    </div>
+                                    <button type="submit" class="btn btn-primary">Отправить</button>
+                                </form>
+                              </body>
+                            </html>'''
+    elif request.method == 'POST':
+        f = request.files['file']
+        img = open(f'static/img/self_image.png', 'wb')
+        img.write(f.read())
+        return "Форма отправлена"
 
 
 if __name__ == '__main__':
