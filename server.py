0d7a00609575cf10d91c82b4daa2934ecfce4704
Lioueq <71265562+Lioueq@users.noreply.github.com>
2021-03-23 14:52:20 +0300

    10

--- server.py
+++ server.py
@@ -1,46 +1,56 @@
-from flask import Flask, url_for, request
-import os
+from flask import Flask, url_for
 
 
 app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/load_photo', methods=['POST', 'GET'])
-def load_photo():
-    if request.method == 'GET':
-        return f'''<!doctype html>
-                            <html lang="en">
-                              <head>
-                                <meta charset="utf-8">
-                                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                                 <link rel="stylesheet"
-                                 href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
-                                 integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-                                 crossorigin="anonymous">
-                                <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
-                                <title>Пример загрузки файла</title>
-                              </head>
-                              <body>
-                                <h1>Загрузка фотографии</h1>
-                                <h2>для участия в миссии</h2>
-                                <form class="login_form" method="post" enctype="multipart/form-data">
-                                    <div class="form-group">
-                                        <label for="photo">Приложите фотографию</label>
-                                        <input type="file" class="form-control-file" id="photo" name="file">
-                                    </div>
-                                    <div class="form-group">
-                                        <img src="static/img/self_image.png" height="400">
-                                    </div>
-                                    <button type="submit" class="btn btn-primary">Отправить</button>
-                                </form>
-                              </body>
-                            </html>'''
-    elif request.method == 'POST':
-        f = request.files['file']
-        img = open(f'static/img/self_image.png', 'wb')
-        img.write(f.read())
-        return "Форма отправлена"
+@app.route('/carousel')
+def carousel():
+    return f"""<!DOCTYPE html>
+                <html lang="en">
+                <head>
+                  <title>Пейзажи Марса</title>
+                  <meta charset="utf-8">
+                  <meta name="viewport" content="width=device-width, initial-scale=1">
+                  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+                  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+                  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+                  <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                </head>
+                <body>
+                <div class="container">
+                  <h1>Пейзажи Марса</h1>  
+                  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                      <li data-target="#myCarousel" data-slide-to="1"></li>
+                      <li data-target="#myCarousel" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                      <div class="item active">
+                        <img src="static/img/mars1.jpg" alt="1" style="width:1600px; height:600px;">
+                      </div>
+                      <div class="item">
+                        <img src="static/img/mars2.jpg" alt="2" style="width:1600px; height:600px;">
+                      </div>
+                      <div class="item">
+                        <img src="static/img/mars3.jpg" alt="3" style="width:1600px; height:600px;">
+                      </div>
+                    </div>
+                    <!-- Left and right controls -->
+                    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                      <span class="glyphicon glyphicon-chevron-left"></span>
+                      <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                      <span class="glyphicon glyphicon-chevron-right"></span>
+                      <span class="sr-only">Next</span>
+                    </a>
+                  </div>
+                </div>
+                </body>
+                </html>"""
 
 
 if __name__ == '__main__':
