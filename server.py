67340dc3e1b40980c1f4e7827b7a908ab961c225
Lioueq <71265562+Lioueq@users.noreply.github.com>
2021-03-24 17:45:17 +0300

    17

--- server.py
+++ server.py
@@ -1,6 +1,15 @@
-from flask import Flask, render_template
-import json
-from random import choice
+from flask import Flask, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    astronaut_id = StringField('Id астронавта', validators=[DataRequired()])
+    astronaut_password = PasswordField('Пароль астронавта', validators=[DataRequired()])
+    captain_id = StringField('Id капитана', validators=[DataRequired()])
+    captain_password = PasswordField('Пароль капитана', validators=[DataRequired()])
+    submit = SubmitField('Доступ')
 
 
 app = Flask(__name__)
@@ -8,12 +17,12 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 @app.route('/')
-@app.route('/member')
-def member():
-    with open('templates/crew.json', 'r', encoding='utf-8') as f:
-        data = json.load(f)
-        member = choice(data['member'])
-    return render_template('member.html', **member)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', title='Аварийный доступ', form=form)
 
 
 if __name__ == '__main__':
