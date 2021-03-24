36ec229affcecef615d944adf43910fa8361ee0c
Lioueq <71265562+Lioueq@users.noreply.github.com>
2021-03-24 13:22:51 +0300

    14

--- server.py
+++ server.py
@@ -1,29 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    astronaut_id = StringField('Id астронавта', validators=[DataRequired()])
+    astronaut_password = PasswordField('Пароль астронавта', validators=[DataRequired()])
+    captain_id = StringField('Id капитана', validators=[DataRequired()])
+    captain_password = PasswordField('Пароль капитана', validators=[DataRequired()])
+    submit = SubmitField('Доступ')
 
 
 app = Flask(__name__)
-
-param = {
-    'title': 'Анкета',
-    'surname': 'Watny',
-    'name': 'Mark',
-    'education': 'выше среднего',
-    'profession': 'штурман марсохода',
-    'sex': 'male',
-    'motivation': 'Всегда мечтал застрять на Марсе!',
-    'ready': 'True'
-}
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 @app.route('/')
-@app.route('/answer')
-def answer():
-    return render_template('auto_answer.html', **param)
-
-
-@app.route('/auto_answer')
-def auto_answer():
-    return render_template('auto_answer.html', **param)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', title='Аварийный доступ', form=form)
 
 
 if __name__ == '__main__':
